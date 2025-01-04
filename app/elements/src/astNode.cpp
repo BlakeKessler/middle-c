1642c2dba34809b29eea32d920951c92ec9c7715
BlakeKessler <blakekessler123@gmail.com>
2025-01-04 18:47:19 -0500

    [CLEF] Parser - define casting functions

--- app/elements/src/astNode.cpp
+++ app/elements/src/astNode.cpp
@@ -3,4 +3,33 @@
 
 #include "astNode.hpp"
 
+//derived to base
+void clef::astNode::downCast(NodeType newType) {
+   if (canDownCastTo(_nodeType, newType)) {
+      _nodeType = newType;
+   } else {
+      throwError(ErrCode::BAD_NODE_CAST, "attempt to downcast from %hhu to %hhu", +_nodeType, +newType);
+   }
+}
+//base to derived
+void clef::astNode::upCast(NodeType newType) {
+   if (canDownCastTo(newType, _nodeType)) {
+      _nodeType = newType;
+   } else {
+      throwError(ErrCode::BAD_NODE_CAST, "attempt to upcast from %hhu to %hhu", +_nodeType, +newType);
+   }
+}
+//derived to base or base to derived
+void clef::astNode::verticalCast(NodeType newType) {
+   if (canDownCastTo(_nodeType, newType) || canDownCastTo(newType, _nodeType)) {
+      _nodeType = newType;
+   } else {
+      throwError(ErrCode::BAD_NODE_CAST, "attempt to vertical cast from %hhu to %hhu", +_nodeType, +newType);
+   }
+}
+//cast without type checking
+void clef::astNode::anyCast(NodeType newType) {
+   _nodeType = newType;
+}
+
 #endif //AST_NODE_CPP
