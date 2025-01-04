84bd2a6ce9d7195eebfdc8f1a53cb33b6daed941
BlakeKessler <blakekessler123@gmail.com>
2025-01-04 18:46:35 -0500

    [CLEF] Parser - add a parameter to clef::Variable nodes to store their value

--- app/elements/header/ast-nodes/Variable.hpp
+++ app/elements/header/ast-nodes/Variable.hpp
@@ -8,16 +8,17 @@
 struct clef::Variable : public clef::Identifier {
    private:
       Type* _type;
-
+      Expr* _val;
    public:
       static constexpr NodeType nodeType() { return NodeType::VAR; }
 
-      Variable():Identifier{},_type{} {}
-      Variable(Type* type):Identifier{},_type{type} {}
-      Variable(Type* type, Identifier* name):Identifier{name?*name:Identifier{}},_type{type} {}
+      Variable(Type* type = {}, Identifier* name = {}, Expr* val = {}):Identifier{name?*name:Identifier{}},_type{type},_val{val} {}
 
       Type*& type() { return _type; }
       const Type* type() const { return _type; }
+
+      Expr*& val() { return _val; }
+      const Expr* val() const { return _val; }
 };
 
 #endif //VARIABLE_HPP
