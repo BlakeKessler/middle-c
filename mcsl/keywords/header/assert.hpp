e471421e4c6cdb7feb93fb166a66c8903d76681f
BlakeKessler <blakekessler123@gmail.com>
2025-01-12 01:13:03 -0500

    [mcsl] unreachable - UNREACHABLE macro and mcsl::__unreachable function

--- mcsl/keywords/header/assert.hpp
+++ mcsl/keywords/header/assert.hpp
@@ -10,13 +10,17 @@
 #ifndef debug_assert
    #ifndef NDEBUG
       #define debug_assert(expr) assert(expr)
+      #define UNREACHABLE mcsl::__unreachable(__FILE__, __LINE__, __func__)
    #else
       #define debug_assert(expr) void(0)
+      #define UNREACHABLE std::unreachable()
+      #include <utility>
    #endif
 #endif
 
 namespace mcsl {
    void __assert_fail(const char* file, const uint line, const char* func, const char* exprStr);
+   void __unreachable(const char* file, const uint line, const char* func);
 };
 
 #endif //MCSL_ASSERT_HPP
