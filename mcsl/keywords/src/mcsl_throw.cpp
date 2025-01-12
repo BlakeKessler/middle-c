fe2712a3990882bd0b8e6305fbc8bb7a0d504660
BlakeKessler <blakekessler123@gmail.com>
2025-01-12 01:17:29 -0500

    [mcsl] errors - update error type strings

--- mcsl/keywords/src/mcsl_throw.cpp
+++ mcsl/keywords/src/mcsl_throw.cpp
@@ -6,8 +6,12 @@
 //!array of MCSL error message prefix strings
 const char* mcsl::ERR_MSG_ARR[] {
    "UNSPECIFIED ERROR: ",
+   "Assertion failed: ",
+   "Unreachable statement reached: ",
    "Segmentation fault: ",
-   "Allocation failure: "
+   "Allocation failure: ",
+   "Integer Overflow: ",
+   "String to Integer Parsing Failure: "
 };
 
 #endif //MCSL_ERR_CPP
