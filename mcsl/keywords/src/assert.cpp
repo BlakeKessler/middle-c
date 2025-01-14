acd06658b87221ef5d81d299841c0369a1dd8052
BlakeKessler <blakekessler123@gmail.com>
2025-01-13 21:57:58 -0500

    [mcsl] unreachable - remove duplicate information in error reporting

--- mcsl/keywords/src/assert.cpp
+++ mcsl/keywords/src/assert.cpp
@@ -5,10 +5,12 @@
 #include "mcsl_throw.hpp"
 
 void mcsl::__assert_fail(const char* file, const uint line, const char* func, const char* exprStr) {
-   mcsl::mcsl_throw(ErrCode::ASSERT_FAIL, "%s:%u: %s: Assertion `%s` failed.", file, line, func, exprStr);
+   mcsl::mcsl_throw(ErrCode::ASSERT_FAIL, "%s:%u: %s: `%s`", file, line, func, exprStr);
+   // mcsl::mcsl_throw(ErrCode::ASSERT_FAIL, "%s:%u: %s: Assertion `%s` failed.", file, line, func, exprStr);
 }
 void mcsl::__unreachable(const char* file, const uint line, const char* func) {
-   mcsl::mcsl_throw(ErrCode::UNREACHABLE_REACHED, "%s:%u: %s: Reached unreachable statement. Aborting.", file, line, func);
+   mcsl::mcsl_throw(ErrCode::UNREACHABLE_REACHED, "%s:%u: %s", file, line, func);
+   // mcsl::mcsl_throw(ErrCode::UNREACHABLE_REACHED, "%s:%u: %s: Reached unreachable statement. Aborting.", file, line, func);
 }
 
 #endif //MCSL_ASSERT_CPP
