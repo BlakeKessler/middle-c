7288593737b10fc146215b32554804294acba9d3
BlakeKessler <blakekessler123@gmail.com>
2025-01-18 20:40:59 -0500

    [Lexer] char literals - add defines for the maximum number of digits in numeric character literals

--- app/CLEF_DEFINES.hpp
+++ app/CLEF_DEFINES.hpp
@@ -14,6 +14,9 @@
 #define OP_DECODER_HASH_BASE 37
 
 //language parameters
-
+#define MAX_BIN_CHAR_LIT_DIGITS 8
+#define MAX_OCT_CHAR_LIT_DIGITS 3
+#define MAX_DEC_CHAR_LIT_DIGITS 3
+#define MAX_HEX_CHAR_LIT_DIGITS 2
 
 #endif //CLEF_DEFINES_HPP
