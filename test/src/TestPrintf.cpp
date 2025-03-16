70bb514ee4a5ee33d23161c6aa48ecd679ded547
BlakeKessler <blakekessler123@gmail.com>
2025-03-15 22:23:05 -0400

    more %f tests

--- test/src/TestPrintf.cpp
+++ test/src/TestPrintf.cpp
@@ -22,10 +22,18 @@ int main() {
 
 
    mcslout.printf(_FMT_("%f\n"), 1.0);
+   mcslout.printf(_FMT_("%.0f\n"), 0.9);
    mcslout.printf(_FMT_("%.6e\n"), 1.0);
    mcslout.printf(_FMT_("%.6e\n"), 0.0);
    mcslout.printf(_FMT_("%.6e\n"), -1.0);
+   mcslout.printf(_FMT_("%.0e\t|\t%.6e\n"), 0.9, 0.9);
+   mcslout.printf(_FMT_("%.6e\n"), -11.0);
+   mcslout.printf(_FMT_("%.6e\n"), 10.0);
+   mcslout.printf(_FMT_("%.6e\t|\t%.6e\n"), 100.0, 0.1);
+   mcslout.printf(_FMT_("%.6e\n"), 9.9);
    mcslout.printf(_FMT_("%.6e\n"), 3.0);
+   mcslout.printf(_FMT_("%.6,16e\n"), 15.0);
+   mcslout.printf(_FMT_("%.6,16e\n"), 17.0);
 
    // mcslout.flush(); //should be handled by the destructor
 }
