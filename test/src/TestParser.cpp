8deb0387a4b4f21673b2a577f2d968a6d25e8484
BlakeKessler <blakekessler123@gmail.com>
2025-05-21 16:28:58 -0400

    simplify parser test file

--- test/src/TestParser.cpp
+++ test/src/TestParser.cpp
@@ -6,45 +6,22 @@
 #include "dyn_arr.hpp"
 #include "io.hpp"
 
-static const auto BIG_HEADER = mcsl::FMT("\033[;1m=======================\n\033[0m");
-static const auto SMALL_HEADER = mcsl::FMT("\033[;1m-----------------------\n\033[0m");
+static const auto BIG_HEADER = mcsl::FMT("\033[;1m==============================================\n\033[0m");
+static const auto SMALL_HEADER = mcsl::FMT("\033[;1m----------------------------------------------\n\033[0m");
 
 int main(const int argc, char** argv) {
-   if (argc < 2) {
-      mcsl::printf(mcsl::FMT("No file path provided. Exiting.\n"));
-      return EXIT_FAILURE;
+   if (argc == 1) {
+      mcsl::printf(FMT("No paths to files to parse provided."));
    }
+   mcsl::printf(BIG_HEADER);
+   for (int i = 1; i < argc; ++i) {
+      //read and tokenize file
+      clef::SourceTokens tokens = clef::Lexer::LexFile(argv[i]);
 
-   //read and tokenize file
-   mcsl::printf(mcsl::FMT("%s"), BIG_HEADER);
-   clef::SourceTokens tokens = clef::Lexer::LexFile(argv[1]);
-   mcsl::printf(mcsl::FMT("\033[1mTokens:\033[22m\n%s"), SMALL_HEADER);
-   mcsl::printf(mcsl::FMT("%s"), tokens);
-   mcsl::printf(mcsl::FMT("\n%s"),BIG_HEADER);
-
-   //abstract syntax tree
-   clef::SyntaxTree tree{};
-   clef::Parser::parse(tokens, tree);
-   mcsl::printf(mcsl::FMT("\033[1mAbstract Syntax Tree:\033[22m\n%s"), SMALL_HEADER);
-   mcsl::printf(mcsl::FMT("%s"), tree);
-   mcsl::printf(mcsl::FMT("\n%s\n"), BIG_HEADER);
-
-   // mcsl::printf(mcsl::FMT("%s\n"), clef::astTNB<clef::Function>{tree, 15});
-   // mcsl::printf(mcsl::FMT("%s\n"), clef::astTNB<clef::Function>{tree, 2});
-
-   //offer to print debug info
-   mcsl::printf(mcsl::FMT("quit (q); print parser debug info (p): "));
-   char ch = mcsl::read();
-   mcsl::printf(mcsl::FMT("\n%s\n"), BIG_HEADER);
-   switch (ch) {
-      case 'q':
-         break;
-      case 'p':
-         tree.print(mcsl::stdout);
-         mcsl::printf(mcsl::FMT("\n%s\n"), BIG_HEADER);
-         break;
-      default:
-         break;
+      //abstract syntax tree
+      clef::SyntaxTree tree{};
+      clef::Parser::parse(tokens, tree);
+      mcsl::printf(mcsl::FMT("\033[1m%s:\033[22m\n%s%s\n%s"), FMT(argv[i]), SMALL_HEADER, tree, BIG_HEADER);
    }
 
    return EXIT_SUCCESS;
