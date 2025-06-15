21442b5496107fc2f27c6ac25100efeba05d0b22
BlakeKessler <blakekessler123@gmail.com>
2025-06-14 23:10:53 -0400

    updated mcsl tests

--- test/src/TestMap.cpp
+++ test/src/TestMap.cpp
@@ -2,29 +2,24 @@
 #include "io.hpp"
 #include "str_slice.hpp"
 
-ulong hash(const mcsl::str_slice& str) {
-   constexpr uint p = 31;
-   constexpr uint m = 1e9 + 9;
-   ulong hash_value = 0;
-   ulong p_pow = 1;
-   for (char c : str) {
-      hash_value = (hash_value + (c - 'a' + 1) * p_pow) % m;
-      p_pow = (p_pow * p) % m;
+void print(const mcsl::map<mcsl::str_slice, uint>& map) {
+   mcsl::printf(FMT("%u\n"), map.size());
+   for (auto it = map.begin(); it != map.end(); ++it) {
+      mcsl::flush();
+      mcsl::printf(FMT("map[\"%s\"] = %u\n"), it->first, it->second);
    }
-   return hash_value;
-}
-bool eq(const mcsl::str_slice& lhs, const mcsl::str_slice& rhs) {
-   return lhs == rhs;
-}
-
-int main() {
-   mcsl::map<mcsl::str_slice, uint, hash, eq> map;
-   map.emplace(mcsl::make_tuple((char*)("vwxyz"),5), mcsl::make_tuple(1));
-   map.emplace(mcsl::make_tuple((char*)("ABCDE"),5), mcsl::make_tuple(2));
-   map.emplace(mcsl::make_tuple((char*)("qwertyuiop"),10), mcsl::make_tuple(3));
-
+   mcsl::printf(FMT("\n"));
    mcsl::printf(FMT("%s: %u\n"), map.contains(FMT("vwxyz")), map[FMT("vwxyz")]);
    mcsl::printf(FMT("%s: %u\n"), map.contains(FMT("ABCDE")), map[FMT("ABCDE")]);
    mcsl::printf(FMT("%s: %u\n"), map.contains(FMT("qwertyuiop")), map[FMT("qwertyuiop")]);
    mcsl::printf(FMT("%s\n"), map.contains(FMT("I'm not in here")));
+}
+
+int main() {
+   mcsl::map<mcsl::str_slice, uint> map;
+   map.emplace(mcsl::make_tuple((char*)("vwxyz"),5), mcsl::make_tuple(1));
+   map.emplace(mcsl::make_tuple((char*)("ABCDE"),5), mcsl::make_tuple(2));
+   map.emplace(mcsl::make_tuple((char*)("qwertyuiop"),10), mcsl::make_tuple(3));
+
+   print(map);
 }
--- test/src/TestSet.cpp
+++ test/src/TestSet.cpp
@@ -2,29 +2,18 @@
 #include "io.hpp"
 #include "str_slice.hpp"
 
-ulong hash(const mcsl::str_slice& str) {
-   constexpr uint p = 31;
-   constexpr uint m = 1e9 + 9;
-   ulong hash_value = 0;
-   ulong p_pow = 1;
-   for (char c : str) {
-      hash_value = (hash_value + (c - 'a' + 1) * p_pow) % m;
-      p_pow = (p_pow * p) % m;
-   }
-   return hash_value;
-}
-bool eq(const mcsl::str_slice& lhs, const mcsl::str_slice& rhs) {
-   return lhs == rhs;
-}
-
-int main() {
-   mcsl::set<mcsl::str_slice, hash, eq> set;
-   set.emplace(FMT("vwxyz"));
-   set.emplace(FMT("ABCDE"));
-   set.emplace(FMT("qwertyuiop"));
-
+void print(const mcsl::set<mcsl::str_slice>& set) {
    mcsl::printf(FMT("%s\n"), set.contains(FMT("vwxyz")));
    mcsl::printf(FMT("%s\n"), set.contains(FMT("ABCDE")));
    mcsl::printf(FMT("%s\n"), set.contains(FMT("qwertyuiop")));
    mcsl::printf(FMT("%s\n"), set.contains(FMT("I'm not in here")));
+}
+
+int main() {
+   mcsl::set<mcsl::str_slice> set;
+   set.emplace(FMT("vwxyz"));
+   set.emplace(FMT("ABCDE"));
+   set.emplace(FMT("qwertyuiop"));
+
+   print(set);
 }
