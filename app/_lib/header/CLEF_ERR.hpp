c9ae337e40dab0fec6dba14ff2b06af6ee6cd7eb
BlakeKessler <blakekessler123@gmail.com>
2025-06-18 23:44:24 -0400

    improved error logging function

--- app/_lib/header/CLEF_ERR.hpp
+++ app/_lib/header/CLEF_ERR.hpp
@@ -9,7 +9,7 @@
 namespace clef {
    extern const mcsl::str_slice ERR_MSG_ARR[];
    [[noreturn]] void throwError(const clef::ErrCode code, const mcsl::str_slice formatStr, auto&&... args);
-   [[noreturn]] void throwError(const clef::ErrCode code, const uint lineNum, const mcsl::str_slice formatStr, auto&&... args);
+   [[noreturn]] void throwError(const clef::ErrCode code, const uint lineNum, const mcsl::str_slice line, const mcsl::str_slice tok, const mcsl::str_slice path, const mcsl::str_slice formatStr, auto&&... args);
 }
 
 //!CLEF formatted error thrower
@@ -27,13 +27,31 @@ namespace clef {
    #endif
 }
 //!CLEF formatted error thrower with line num
-[[noreturn]] void clef::throwError(const clef::ErrCode code, const uint lineNum, const mcsl::str_slice formatStr, auto&&... args) {
+[[noreturn]] void clef::throwError(const clef::ErrCode code, const uint lineNum, const mcsl::str_slice line, const mcsl::str_slice tok, const mcsl::str_slice path, const mcsl::str_slice formatStr, auto&&... args) {
    mcsl::printf(mcsl::FMT("\n"));
    mcsl::stdout.flush();
-   mcsl::err_printf(mcsl::FMT("\033[31;1;4mCLEF ERROR:\033[0m %s"), ERR_MSG_ARR[+code]);
+   // mcsl::err_printf(mcsl::FMT("\033[31;1;4mCLEF ERROR:\033[0m %s"), ERR_MSG_ARR[+code]);
+   // mcsl::err_printf(formatStr, args...);
+   // mcsl::err_printf(mcsl::FMT(" \033[35m(line %u)\033[0m\n"), lineNum);
+   
+   debug_assert(tok.begin() >= line.begin());
+   debug_assert(tok.end() <= line.end());
+   const mcsl::str_slice beforeTok = mcsl::str_slice::make(line.begin(), tok.begin());
+   const mcsl::str_slice afterTok = mcsl::str_slice::make(tok.end(), line.end());
+   
+   mcsl::err_printf(mcsl::FMT("\033[1m%s:%u:%u: \033[31mERROR:\033[39m "), path, lineNum, beforeTok.size());
    mcsl::err_printf(formatStr, args...);
-   mcsl::err_printf(mcsl::FMT(" \033[35m(line %u)\033[0m\n"), lineNum);
+   mcsl::err_printf(mcsl::FMT(" [%s, %u]\033[22m\n"), ERR_MSG_ARR[+code], +code);
+   uint spaces = mcsl::err_printf(mcsl::FMT("    %u "));
+   uint spaces2 = mcsl::err_printf(mcsl::FMT("| %s"), lineNum, beforeTok) - 1;
+   mcsl::err_printf(mcsl::FMT("\033[4m%s\033[24m%s\n"), tok, afterTok);
+   mcsl::stderr.write(' ', spaces);
+   mcsl::stderr.write('|');
+   mcsl::stderr.write(' ', spaces2);
+   mcsl::err_printf(mcsl::FMT("\033[1;32m^\033[22;39m\n"));
+
    mcsl::stderr.flush();
+   
    #ifdef NDEBUG
       std::exit(EXIT_FAILURE);
    #else
